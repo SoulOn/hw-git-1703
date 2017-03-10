9bfb84b635aff035868843afaf2eb4d1c0a4c18a
Saulius Sutkus <s.sutkus@gmail.com>
2017-03-10 11:55:14 +0200

    Created: header.php

---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Studio45</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700|Raleway:300,400,600,700" rel="stylesheet">
+</head>
+
+
+
+<body>
+<!-- Header -->
+<header>
+    <div class="header-container">
+        <div class="logo-box">
+            <img src="img/logo.svg">
+        </div>
+
+        <div class="menu-box">
+            <ul>
+                <li><a href="#">Pagrindinis</a></li>
+                <li><a href="#">Baldai</a></li>
+                <li><a href="#">Interjero elementai</a></li>
+                <li><a href="#">Projektai</a></li>
+                <li><a href="#">Apie mus</a></li>
+                <li><a href="#">Kontaktai</a></li>
+            </ul>
+        </div>
+    </div>
+</header>
