bb9d60f949737fd63eabd84f7f9febee9a0b0f97
Saulius Sutkus <s.sutkus@gmail.com>
2017-03-10 12:33:42 +0200

    changed copyright line

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer id="footer" role="contentinfo">
     <div id="copyright">
         <div class="container">
-            © 2017 Studio45. Visos teisės saugomos.
+            © 2017 Studio45. Visos teisės saugomos. <span>designed by Design Partisans</span>
         </div>
     </div>
 </footer>
