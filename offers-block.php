2fb6e6ca92b632eca722fa330613535b39a63cab
Saulius Sutkus <s.sutkus@gmail.com>
2017-03-10 11:59:13 +0200

    Created: offers-block.php

---
+++ offers-block.php
@@ -0,0 +1,79 @@
+<!-- Products offer -->
+<div class="products-container">
+    <h2>Nauja 2017 metų pavasario baldų kolecija</h2>
+    <h4>Baldai kurie pražys ansčiau nei pavasarinės gėlės Jūsų namuose</h4>
+
+    <div class="products-block">
+
+        <div class="product-item"><!-- 1st -->
+            <img class="product-image" src="img/P1.jpg">
+            <span class="product-category">Rašomasis stalas</span>
+            <h3>Fenix </h3>
+
+            <ul class="colors">
+                <li style="background-color:#502100;"></li>
+                <li style="background-color:#a17d64;"></li>
+                <li style="background-color:#dcc59d;"></li>
+            </ul>
+
+            <ul class="product-block-footer">
+                <li class="price">359€</li>
+                <li class="preview-button"><a href="#">Apžiūrėti</a></li>
+            </ul>
+        </div>
+
+        <div class="product-item"><!-- 2nd -->
+            <img class="product-image" src="img/P2.jpg">
+            <span class="product-category">Kėdė</span>
+            <h3>Cherry </h3>
+
+            <ul class="colors">
+                <li style="background-color:#5f92a2;"></li>
+                <li style="background-color:#a29b5f;"></li>
+                <li style="background-color:#dc9d9d;"></li>
+                <li style="background-color:#ff5a00;"></li>
+                <li style="background-color:#00518a;"></li>
+            </ul>
+
+            <ul class="product-block-footer">
+                <li class="price">189€</li>
+                <li class="preview-button"><a href="#">Apžiūrėti</a></li>
+            </ul>
+        </div>
+
+        <div class="product-item"><!-- 3rd -->
+            <img class="product-image" src="img/P3.jpg">
+            <span class="product-category">Sofa</span>
+            <h3>Ocean </h3>
+
+            <ul class="colors">
+                <li style="background-color:#313a4b;"></li>
+                <li style="background-color:#5f92a2;"></li>
+                <li style="background-color:#a17d64;"></li>
+            </ul>
+
+            <ul class="product-block-footer">
+                <li class="price">779€</li>
+                <li class="preview-button"><a href="#">Apžiūrėti</a></li>
+            </ul>
+        </div>
+
+        <div class="product-item"><!-- 4th -->
+            <img class="product-image" src="img/P4.jpg">
+            <span class="product-category">Indaija</span>
+            <h3>Rectangular</h3>
+
+            <ul class="colors">
+                <li style="background-color:#ffffff; border: 1px solid #efefef;"></li>
+                <li style="background-color:#ffea00;"></li>
+                <li style="background-color:#09cbcd;"></li>
+            </ul>
+
+            <ul class="product-block-footer">
+                <li class="price">429€</li>
+                <li class="preview-button"><a href="#">Apžiūrėti</a></li>
+            </ul>
+        </div>
+    </div>
+    <a class="button" href="#">Visa kolekcija</a>
+</div>
