cd019dc9f2f01405803c81f854fea544e3e2699c
Saulius Sutkus <s.sutkus@gmail.com>
2017-03-10 12:03:28 +0200

    Created: blog-block.php

---
+++ blog-block.php
@@ -0,0 +1,54 @@
+<div class="blog-container">
+    <div class="posts-block">
+        <div class="post-box">
+            <article>
+                <div>
+                    <span>2016 rugsėjo 30</span>
+                    <h2>Apie baldus ir interjerą</h2>
+                    <p>Čia galite rasti ne tik naujienas apie baldus ir jų tendencijas, bet ir patarimų: kaip sukūrti harmoningą interjerą, kokias medžiagas pasirinti baldo gamybai ar ka daryti, kad virtuvėje būtų tvarka.</p>
+                    <a href="#">Rodyti visas naujienas</a>
+                </div>
+            </article>
+            <article>
+                <div class="post-info">
+                    <span>2016 rugsėjo 26</span>
+                    <h3>Minimalistiški baldai iš metalo</h3>
+                    <a href="#">Skaityti plačiau</a>
+                </div>
+                <div class="post-img">
+                    <img src="img/blog1.jpg">
+                </div>
+            </article>
+            <article>
+                <div class="post-info">
+                    <span>2016 rugsėjo 3</span>
+                    <h3>Virtuvės baldų medžiagos ir dizainas</h3>
+                    <a href="#">Skaityti plačiau</a>
+                </div>
+                <div class="post-img">
+                    <img src="img/blog2.jpg">
+                </div>
+            </article>
+            <article>
+                <div class="post-info">
+                    <span>2016 rugpjucio 15</span>
+                    <h3>10 idėjų, kaip apšviesti virtuvę</h3>
+                    <a href="#">Skaityti plačiau</a>
+                </div>
+                <div class="post-img">
+                    <img src="img/blog3.jpg">
+                </div>
+            </article>
+            <article>
+                <div class="post-info">
+                    <span>2016 rugpjučio 1</span>
+                    <h3>Skandinaviško stiliaus baldų tendencijos</h3>
+                    <a href="#">Skaityti plačiau</a>
+                </div>
+                <div class="post-img">
+                    <img src="img/blog4.jpg">
+                </div>
+            </article>
+        </div>
+    </div>
+</div>
